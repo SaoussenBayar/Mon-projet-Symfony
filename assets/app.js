f545598507e52bc9930d43d976959466d84d535f
SaoussenBayar <saoussenbayar@gmail.com>
2025-02-25 15:59:26 +0100

    25_02_2025

--- assets/app.js
+++ assets/app.js
@@ -2,14 +2,5 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-
-
-
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
