9f606774f3c8a6fefc28b3d1c2b4fb31edf03792
SaoussenBayar <saoussenbayar@gmail.com>
2025-02-27 10:26:54 +0100

    27_02_2025

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -2,31 +2,44 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegistrationFormType; 
+use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Repository\UserRepository;
+
 class RegistrationController extends AbstractController
 {
     #[Route('/inscription', name: 'app_register', methods: ['GET', 'POST'])]
-    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
-    {
+    public function register(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository $userRepository
+    ): Response {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
     
         if ($form->isSubmitted() && $form->isValid()) {
+            // Vérifier si l'e-mail existe déjà
+            $existingUser = $userRepository->findOneBy(['email' => $user->getEmail()]);
+            if ($existingUser) {
+                $this->addFlash('error', 'Cet e-mail est déjà utilisé.');
+                return $this->redirectToRoute('app_register');
+            }
+
             // Assigner les valeurs manquantes
             $user->setDateInscription(new \DateTime());
-            $user->setRoles(['ROLE_USER']); // Assurer un rôle par défaut
-            $user->setIsVerified(true);  // Marquer l'utilisateur comme vérifié (si applicable)
+            $user->setRoles(['ROLE_USER']);
+            $user->setIsVerified(true);
             
             // Encoder le mot de passe
             $user->setPassword(
-                $passwordHasher->hashPassword(  // Utilise hashPassword
+                $passwordHasher->hashPassword(
                     $user,
                     $form->get('password')->getData()
                 )
@@ -38,7 +51,6 @@ class RegistrationController extends AbstractController
     
             $this->addFlash('success', 'Inscription réussie ! Vous pouvez vous connecter.');
     
-            // Redirection vers la page de connexion ou page d'accueil
             return $this->redirectToRoute('app_login');
         }
     
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -12,6 +12,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
+#[UniqueEntity(fields: ["email"], message: "Cet e-mail est déjà utilisé.")]
+
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
